6141d40b9f6dd638ef95b43570a78e6c53644d5f
Liu Jiuqi <eric2625558982@gmail.com>
2021-05-24 04:25:36 +0200

    Create KingKnightStateTest.java

---
+++ src/test/java/kingKnight/state/KingKnightStateTest.java
@@ -0,0 +1,46 @@
+package kingKnight.state;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KingKnightStateTest {
+
+    @Test
+    void isSolved() {
+    }
+
+    @Test
+    void getKingRow() {
+    }
+
+    @Test
+    void getKingCol() {
+    }
+
+    @Test
+    void getKnightRow() {
+    }
+
+    @Test
+    void getKnightCol() {
+    }
+
+    @Test
+    void getMatrix() {
+    }
+
+    @Test
+    void canMoveToNext() {
+    }
+
+    @Test
+    void moveToNext() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+
+}
